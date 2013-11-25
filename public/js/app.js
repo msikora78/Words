fdeb2e501ba48aeac851eaf293425ea578dd48ef
Michal Sikora <Michal.Sikora@Ticketmaster.com>
2013-11-24 17:53:38 -0700

    Refactor as it was getting way to messy

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,15 @@ $(function($){
 
     $('body').hide();
 
-	window.Draw = window.Draw || {};
-
     var $canvas = $('#overlayCanvas');
 
+    var touchBoard = new Words.TouchBoard($canvas);
+    touchBoard
+        .on('touchDetected',processTouch)
+        .on('touchEnd',processWord);
 
-	var currentTouchPath, currentPoint = null, lastPoint = null, buttonDown = false;
-    var activePen = new Draw.Pen($canvas);
-    var touchAreas = new Words.TouchAreas($canvas.width(),4,0.7);
     var currentWord = {};
     var totalScore = 0;
-
     var socket = io.connect(window.location.origin);
 
     socket.on('message', function (data) {
@@ -29,73 +27,26 @@ $(function($){
         socket.emit('send', { word: word });
     };
 
+    function processTouch(event){
+        var x = event.x, y = event.y;
+        var coords = x + '-' + y;
+        var id = '#letter-cell-'+ coords;
+        $(id).parent().addClass('selected');
 
-    $canvas.on({
-        'vmousedown': function(event){
-            //console.log(event);
-            buttonDown = true;
-            lastPoint = null;
-            currentTouchPath = {
-                stroke: []
-            };
-            var point = draw(event);
-            event.preventDefault();
-        },
-
-        'vmouseup vmouseout': function(event){
-            //console.log(event);
-            buttonDown = false;
-            if(currentTouchPath) {
-                var point = draw(event);
-                lastPoint = null;
-                processWord(currentTouchPath);
-                currentTouchPath = null;
-            }
-            event.preventDefault();
-        },
-
-        'vmousemove': function(event){
-            //console.log(event);
-            if(buttonDown && currentTouchPath) draw (event);
-            event.preventDefault();
+        if(!currentWord[id]) {
+            touchBoard.setLastCell(x, y);
+            var index = _.keys(currentWord).length + 1;
+            currentWord[id] = index;
+            displayWord();
         }
-    });
+    }
 
-    function draw(event){
-        var x = event.offsetX || event.pageX;
-        var y = event.offsetY || event.pageY;
-//        var x = event.clientX;
-//        var y = event.clientY;
-
-        $('#position').text('[' + x + ',' + y + ']');
-
-        currentPoint = {
-            x: x,
-            y: y
-        };
-
-        activePen.drawLine( lastPoint, currentPoint);
-
-        if(lastPoint && currentPoint) {
-            var areas = touchAreas.findIntersects( lastPoint, currentPoint);
-            //console.log('areas:', areas);
-            _.each(areas, function (area) {
-                var coords = area.x + '-' + area.y;
-                var id = '#letter-cell-'+ coords;
-                $(id).parent().addClass('selected');
-
-                if(!currentWord[id]) {
-                    touchAreas.setLastCell(area.x, area.y);
-                    var index = _.keys(currentWord).length + 1;
-                    currentWord[id] = index;
-                    displayWord();
-                }
-            });
-        }
-
-        lastPoint = currentPoint;
-        currentTouchPath.stroke.push(currentPoint);
-        return currentPoint;
+    function processWord(request) {
+        var word = getWord().toLowerCase();
+        checkMyWord(word);
+        $('.letter-cell').removeClass('selected');
+        currentWord = {};
+        displayWord();
     }
 
     function getWord() {
@@ -151,21 +102,12 @@ $(function($){
         }
     }
 
-    function processWord(request) {
-        var word = getWord().toLowerCase();
-        checkMyWord(word);
-        activePen.clearCanvas();
-        $('.letter-cell').removeClass('selected');
-        currentWord = {};
-        displayWord();
-        touchAreas.resetLastCell();
-    }
 
     var board = [], words = {}, usedWords = {}, letterScoring = {};
 
     $.ajax('/board').done(render);
 
-    function render(resp){
+    function render(resp) {
         board = resp.board;
         letterScoring = resp.letterScoring;
         _.each(resp.words, function(word) {
@@ -175,7 +117,6 @@ $(function($){
         var tpl = $('#letters-tmpl').html();
         var html = _.template(tpl, resp, {variable: 'd'});
         $('#board').html(html);
-        console.log(html);
         $('body').show();
     }
 
--- public/js/util/touchBoard.js
+++ public/js/util/touchBoard.js
@@ -6,12 +6,93 @@ $(function($){
 
     Words.TouchBoard = Class.extend({
         init: function(canvas) {
-            var currentTouchPath, currentPoint = null, lastPoint = null, buttonDown = false;
-            var activePen = new Draw.Pen(canvas);
-            var touchAreas = new Words.TouchAreas($canvas.width(),4,0.7);
+            this.canvas = canvas;
+            this.currentTouchPath = {};
+            this.currentPoint = null;
+            this.lastPoint = null;
+            this.buttonDown = false;
+            this.activePen = new Draw.Pen(canvas);
+            this.touchAreas = new Words.TouchAreas(canvas.width(),4,0.7);
 
+            _.bindAll(this);
+
+            canvas.on({
+                'vmousedown': this.beginTouch,
+                'vmousemove': this.moveTouch,
+                'vmouseup vmouseout': this.endTouch
+            });
+        },
+
+        beginTouch: function(event){
+            //console.log(event);
+            this.buttonDown = true;
+            this.lastPoint = null;
+            this.currentTouchPath = {
+                stroke: []
+            };
+            this.draw(event);
+            event.preventDefault();
+        },
+
+        moveTouch: function(event){
+            //console.log(event);
+            if(this.buttonDown && this.currentTouchPath) {
+                this.draw (event);
+            }
+            event.preventDefault();
+        },
+
+        endTouch: function(event){
+            this.buttonDown = false;
+            if(this.currentTouchPath) {
+                this.draw(event);
+                this.lastPoint = null;
+                this.endPath();
+                this.currentTouchPath = null;
+            }
+            event.preventDefault();
+        },
+
+        draw: function(event){
+            var x = event.offsetX || event.pageX;
+            var y = event.offsetY || event.pageY;
+    //        var x = event.clientX;
+    //        var y = event.clientY;
+
+            this.currentPoint = {
+                x: x,
+                y: y
+            };
+
+            this.activePen.drawLine( this.lastPoint, this.currentPoint);
+
+            if(this.lastPoint && this.currentPoint) {
+                var areas = this.touchAreas.findIntersects( this.lastPoint, this.currentPoint);
+                //console.log('areas:', areas);
+                var _this = this;
+                _.each(areas, function (area) {
+                    var coords = area.x + '-' + area.y;
+                    _this.trigger('touchDetected',{ x: area.x, y: area.y });
+                });
+            }
+
+            this.lastPoint = this.currentPoint;
+            this.currentTouchPath.stroke.push(this.currentPoint);
+        },
+
+        endPath: function () {
+            this.activePen.clearCanvas();
+            this.touchAreas.resetLastCell();
+            this.trigger('touchEnd');
+        },
+
+        setLastCell: function(x,y){
+            this.touchAreas.setLastCell(x,y);
         }
 
+
     });
+
+    BackboneEvents.mixin(Words.TouchBoard.prototype);
 });
 
